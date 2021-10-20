863ec62a59e4165c405dc5d0bf28efae8f750c9f
Jan-Benedikt Jagusch <jan.jagusch@gmail.com>
2021-10-20 12:31:45 +0200

    add error_reporting_v2 and deprecate error_reporting

--- cloud_functions_utils/error_reporting.py
+++ cloud_functions_utils/error_reporting.py
@@ -2,7 +2,39 @@
 This module provides a decorator class to report errors to Google Cloud.
 """
 
+import functools
+import logging
+import warnings
+from typing import Callable, Optional
+
 from google.cloud.error_reporting import Client
+
+
+def error_reporting_v2(*, raise_: Optional[bool] = True) -> Callable:
+    """
+    Create decorator that adds error reporting to a function.
+
+    Reports errors to Stackdriver and to function logs.
+
+    Args:
+        raise_ (Optional[bool]): Whether to raise the error after it has been logged.
+
+    Returns:
+        Callable: A decorator.
+    """
+    def decorator(func: Callable):
+        client = Client()
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            try:
+                func(*args, **kwargs)
+            except BaseException as error:
+                client.report_exception()
+                logging.error(error)
+                if raise_:
+                    raise error
+        return wrapper
+    return decorator
 
 
 # pylint: disable=invalid-name
@@ -13,6 +45,10 @@ class error_reporting:
     Args:
         func (callable): The function you want to decorate.
     """
+    warnings.warn(
+        "This function is deprecated and will be removed in a future release. "
+        "Please use `error_reporting_v2` instead."
+    )
 
     def __init__(self, func):
         self._func = func
